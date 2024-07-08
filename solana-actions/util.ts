b07e72ac8501d9b14579f2b605fca62ceda92d8b
nardevvv <vedran.sisic+personal@gmail.com>
2024-07-08 14:53:31 -0400

    Add method to get tensor name

--- solana-actions/util.ts
+++ solana-actions/util.ts
@@ -1,4 +1,5 @@
-export const TORQUE_API_URL: string = process.env.TORQUE_API_URL ?? "https://api.torque.so";
+export const TORQUE_API_URL: string =
+  process.env.TORQUE_API_URL ?? "https://api.torque.so";
 
 const tensorGraphqlCollectionQuery = `query CollectionStats($slug: String!) {
   instrumentTV2(slug: $slug) {
@@ -10,24 +11,49 @@ const tensorGraphqlCollectionQuery = `query CollectionStats($slug: String!) {
     name
   }
 }`;
+
 export const getTensorSlugFromCollectionAddress = async (
-    collectionAddress: string,
-    tensorApiKey?: string
+  collectionAddress: string,
+  tensorApiKey?: string
 ): Promise<string> => {
-    const response = await fetch("https://api.tensor.so/graphql", {
-        method: "POST",
-        headers: {
-            "X-TENSOR-API-KEY": tensorApiKey ?? process.env.TENSOR_API_KEY as string,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            query: tensorGraphqlCollectionQuery,
-            variables: {
-                "slug": collectionAddress
-            }
-        }),
-        redirect: "follow"
-    });
-    const { data } = await response.json();
-    return data.instrumentTV2.slugDisplay;
-}
+  const response = await fetch("https://api.tensor.so/graphql", {
+    method: "POST",
+    headers: {
+      "X-TENSOR-API-KEY":
+        tensorApiKey ?? (process.env.TENSOR_API_KEY as string),
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: tensorGraphqlCollectionQuery,
+      variables: {
+        slug: collectionAddress,
+      },
+    }),
+    redirect: "follow",
+  });
+  const { data } = await response.json();
+  return data.instrumentTV2.slugDisplay;
+};
+
+export const getTensorNameFromCollectionAddress = async (
+  collectionAddress: string,
+  tensorApiKey?: string
+): Promise<string> => {
+  const response = await fetch("https://api.tensor.so/graphql", {
+    method: "POST",
+    headers: {
+      "X-TENSOR-API-KEY":
+        tensorApiKey ?? (process.env.TENSOR_API_KEY as string),
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: tensorGraphqlCollectionQuery,
+      variables: {
+        slug: collectionAddress,
+      },
+    }),
+    redirect: "follow",
+  });
+  const { data } = await response.json();
+  return data.instrumentTV2.name;
+};
