40848aa0d1ffc1a22955bb37a8fb190f1f856f40
nardevvv <vedran.sisic+personal@gmail.com>
2024-07-17 17:47:12 -0400

    fix token fetch

--- tokens.ts
+++ tokens.ts
@@ -1,31 +1,65 @@
-export async function getSplDetails(tokenAddress: string) {
-    const response = await fetch(process.env.HELIUS_RPC as string, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            jsonrpc: "2.0",
-            id: "string",
-            method: "getAsset",
-            params: {
-                id: tokenAddress,
-            },
-        }),
-    });
-    const { result } = await response.json();
-    if (result) {
-        return {
-            name: result.content.metadata.name,
-            symbol: result.content.metadata.symbol,
-            logo: result.content.links.image,
-            decimals: result.token_info.decimals,
-            usdcValuePerToken: result.token_info.price_info.currency === 'USDC'
-                ? result.token_info.price_info.price_per_token
-                : 1,
+import { CLICKY_ADDRESS } from "./consants.js";
+import { Token } from "./types.js";
 
+export async function getSplDetails(tokenAddress: string): Promise<Token> {
+  if (tokenAddress === CLICKY_ADDRESS) {
+    return {
+      name: "Clicky",
+      logo: "https://torque-assets.s3.us-east-1.amazonaws.com/clicky.png",
+      decimals: 0,
+      symbol: "CLICKY",
+      usdcValuePerToken: 0,
+    };
+  }
+
+  const response = await fetch(process.env.HELIUS_RPC as string, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: "string",
+      method: "getAsset",
+      params: {
+        id: tokenAddress,
+      },
+    }),
+  });
+
+  const { result } = (await response.json()) as unknown as {
+    result: {
+      content: {
+        metadata: {
+          name: string;
+          symbol: string;
         };
-    } else {
-        throw new Error("Token not found");
-    }
+        links: {
+          image: string;
+        };
+      };
+      token_info: {
+        decimals: number;
+        price_info: {
+          currency?: string;
+          price_per_token: number;
+        };
+      };
+    };
+  };
+
+  if (result) {
+    return {
+      name: result.content.metadata.name,
+      symbol: result.content.metadata.symbol,
+      logo: result.content.links.image,
+      decimals: result.token_info.decimals,
+      usdcValuePerToken:
+        result.token_info.price_info.currency === "USDC"
+          ? result.token_info.price_info.price_per_token
+          : 1,
+    };
+  } else {
+    throw new Error("Token not found");
+  }
 }
---
+++ types/tokens.ts
@@ -0,0 +1,7 @@
+export type Token = {
+  name: string;
+  logo: string;
+  decimals: number;
+  symbol: string;
+  usdcValuePerToken: number;
+};
