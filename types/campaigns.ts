7ed9422d0293ad0df87e485e7960108eb0d4c1f8
0xlarry <0xlarry8@gmail.com>
2024-11-29 08:55:04 -0500

    add origin to campaign create body

--- types/campaigns.ts
+++ types/campaigns.ts
@@ -6,58 +6,67 @@ import { EventRequirementConfigSchema } from "./eventConfig";
  * Offer types for campaigns
  */
 export enum OfferType {
-  CLICK = "CLICK", // TODO: EVENT
-  BOUNTY = "BOUNTY", // TODO: ACTION
+    CLICK = "CLICK", // TODO: EVENT
+    BOUNTY = "BOUNTY", // TODO: ACTION
 }
 
 /**
  * Campaign types
  */
 export enum CampaignType {
-  CLICK = "CLICK",
-  BOUNTY = "BOUNTY",
+    CLICK = "CLICK",
+    BOUNTY = "BOUNTY",
 }
 
 /**
  * Offer Themes
  */
 export enum OfferTheme {
-  DARK = "DARK",
-  LIGHT = "LIGHT",
+    DARK = "DARK",
+    LIGHT = "LIGHT",
 }
 
 /**
  * Reward types for campaign conversions
  */
 export enum RewardType {
-  POINTS = "POINTS",
-  TOKENS = "TOKENS",
-  ASYMMETRIC_REWARD = "ASYMMETRIC_REWARD",
+    POINTS = "POINTS",
+    TOKENS = "TOKENS",
+    ASYMMETRIC_REWARD = "ASYMMETRIC_REWARD",
 }
 
 /**
  * Raffle participant types
  */
 export enum RaffleParticipants {
-  USER = "USER",
-  PUBLISHER = "PUBLISHER",
-  BOTH = "BOTH",
+    USER = "USER",
+    PUBLISHER = "PUBLISHER",
+    BOTH = "BOTH",
 }
 
 /**
  * Conversion Audience Operations
  */
 export enum ConversionAudienceOperation {
-  AND = "AND",
-  OR = "OR",
+    AND = "AND",
+    OR = "OR",
+}
+
+/**
+ * Campaign Origins
+ */
+export enum CampaignOrigin {
+    TORQUE = "TORQUE",
+    TRENCHES = "TRENCHES",
+    THIRD_PARTY = "THIRD_PARTY",
 }
 
 /**
  * Conversion audience schema
  */
 export const ConversionAudienceSchema = z.object({
-  campaignIds: z.array(z.string()),
-  operation: z.nativeEnum(ConversionAudienceOperation),
+    campaignIds: z.array(z.string()),
+    operation: z.nativeEnum(ConversionAudienceOperation),
 });
 
 /**
@@ -73,26 +82,26 @@ export type ConversionAudience = z.infer<typeof ConversionAudienceSchema>;
  * Asymmetric Reward Schema
  */
 export const AsymmetricRewardSchema = z.object({
-  /**
-   * The token address to reward
-   */
-  tokenAddress: z.string(),
-  /**
-   * The amount to reward
-   */
-  amount: z.coerce.number(),
-  /**
-   * The user public key that will be populated in the payout transaction
-   */
-  userPubKey: z.string().nullish(),
-  /**
-   * Who can participate in the raffle, users, publishers, or both
-   */
-  participants: z.nativeEnum(RaffleParticipants).nullish(),
-  /**
-   * The transaction hash of the payout transaction
-   */
-  payoutTx: z.string().nullish(),
+    /**
+     * The token address to reward
+     */
+    tokenAddress: z.string(),
+    /**
+     * The amount to reward
+     */
+    amount: z.coerce.number(),
+    /**
+     * The user public key that will be populated in the payout transaction
+     */
+    userPubKey: z.string().nullish(),
+    /**
+     * Who can participate in the raffle, users, publishers, or both
+     */
+    participants: z.nativeEnum(RaffleParticipants).nullish(),
+    /**
+     * The transaction hash of the payout transaction
+     */
+    payoutTx: z.string().nullish(),
 });
 
 /**
@@ -104,16 +113,14 @@ export type AsymmetricReward = z.infer<typeof AsymmetricRewardSchema>;
  * Loot Box Reward Schema
  */
 export const LootBoxRewardSchema = z.object({
-  /**
-   * The token address to reward
-   */
-  tokenAddress: z.string().min(1, "Token address is required"),
-  /**
-   * The groups of rewards to distribute
-   */
-  rewards: z.array(
-    z.object({ amount: z.coerce.number(), users: z.coerce.number() })
-  ),
+    /**
+     * The token address to reward
+     */
+    tokenAddress: z.string().min(1, "Token address is required"),
+    /**
+     * The groups of rewards to distribute
+     */
+    rewards: z.array(z.object({ amount: z.coerce.number(), users: z.coerce.number() })),
 });
 
 /**
@@ -125,31 +132,27 @@ export type LootBoxReward = z.infer<typeof LootBoxRewardSchema>;
  * Loot Box form input schema for creating a new campaign
  */
 export const LootBoxRewardInputSchema = z.discriminatedUnion("enabled", [
-  z
-    .object({
-      /**
-       * Whether the loot box reward is enabled or not
-       */
-      enabled: z.literal(true),
-    })
-    .merge(LootBoxRewardSchema),
-  z
-    .object({
-      /**
-       * Wether the loot box reward is disabled or not
-       */
-      enabled: z.literal(false),
-    })
-    .merge(
-      z.object({
-        tokenAddress: z.string().nullish(),
-        rewards: z
-          .array(
-            z.object({ amount: z.coerce.number(), users: z.coerce.number() })
-          )
-          .nullish(),
-      })
-    ),
+    z
+        .object({
+            /**
+             * Whether the loot box reward is enabled or not
+             */
+            enabled: z.literal(true),
+        })
+        .merge(LootBoxRewardSchema),
+    z
+        .object({
+            /**
+             * Wether the loot box reward is disabled or not
+             */
+            enabled: z.literal(false),
+        })
+        .merge(
+            z.object({
+                tokenAddress: z.string().nullish(),
+                rewards: z.array(z.object({ amount: z.coerce.number(), users: z.coerce.number() })).nullish(),
+            }),
+        ),
 ]);
 
 /**
@@ -161,10 +164,10 @@ export type LootBoxRewardInput = z.infer<typeof LootBoxRewardInputSchema>;
  * Campaign filter schema
  */
 export const CampaignRequestParamsSchema = z.object({
-  limit: z.number().optional(),
-  page: z.number().optional(),
-  status: z.string().optional(),
-  advertiserPubKey: z.string().optional(),
+    limit: z.number().optional(),
+    page: z.number().optional(),
+    status: z.string().optional(),
+    advertiserPubKey: z.string().optional(),
 });
 
 /**
@@ -176,106 +179,108 @@ export type CampaignRequestParams = z.infer<typeof CampaignRequestParamsSchema>;
  * Schema for creating a new campaign
  */
 export const CreateCampaignInputSchema = z.object({
-  /**
-   * The name of the campaign
-   */
-  campaignName: z.string(),
-  /**
-   * The type of campaign
-   */
-  campaignType: z.nativeEnum(CampaignType),
-  /**
-   * A short description of the offer/campaign
-   */
-  campaignDescription: z.string().max(250).nullish(),
-  /**
-   * Full campaign content of the offer/campaign in Markdown
-   * This supports large format content such as images, videos, in Markdown format
-   */
-  campaignContent: z.string().nullish(),
-  /**
-   * The featured image for the offer/campaign
-   */
-  campaignImage: z.string().nullish(),
-  /**
-   * Default launding page for the offer/campaign to link users to
-   */
-  landingPage: z.string(),
-  /**
-   * Deprecated: Whether the offer/campaign is blink only or not
-   */
-  blinkOnly: z.boolean().nullish(),
-  /**
-   * The theme of the offer/campaign
-   */
-  offerTheme: z.nativeEnum(OfferTheme).optional(),
-  /**
-   * The background image for the offer/campaign
-   */
-  offerBgImage: z.string().nullish(),
-  /**
-   * The event requirements for the offer/campaign
-   *
-   * {@link EventRequirementConfig}
-   */
-  eventConfig: z.array(EventRequirementConfigSchema),
-  /**
-   * The total number of conversions
-   */
-  conversionCount: z.coerce.number(),
-  /**
-   * The publisher reward type
-   */
-  publisherRewardType: z.nativeEnum(RewardType).optional(),
-  /**
-   * The publisher token address
-   */
-  publisherTokenAddress: z.string().optional(),
-  /**
-   * The payout per conversion for each referral by a publisher
-   */
-  publisherPayoutPerConversion: z.coerce.number().optional(),
-  /**
-   * The user reward type
-   */
-  userRewardType: z.nativeEnum(RewardType).optional(),
-  /**
-   * The user token address
-   */
-  userTokenAddress: z.string().optional(),
-  /**
-   * The payout per conversion for each user
-   */
-  userPayoutPerConversion: z.coerce.number().optional(),
-  /**
-   * The asymmetric/raffle rewards to distribute
-   *
-   * NOTE: The userPubKey and payoutTx are omitted from schema for campaign creation
-   */
-  asymmetricRewards: z
-    .array(AsymmetricRewardSchema.omit({ userPubKey: true, payoutTx: true }))
-    .optional(),
-  /**
-   * The lootbox rewards to distribute
-   */
-  lootBoxRewards: LootBoxRewardInputSchema.optional(),
-  /**
-   * The start time of the campaign/offer in Unix timestamp
-   */
-  startTime: z.coerce.number(),
-  /**
-   * The end time of the campaign/offer in Unix timestamp
-   */
-  endTime: z.coerce.number(),
-  /**
-   * The audiences that the offer/campaign is available to
-   */
-  audience: z.string().optional().nullable(),
-  /**
-   * If set, the offer can only be completed by user's who have completed
-   * a conversion for the given offers prior
-   */
-  conversionAudience: ConversionAudienceSchema.optional(),
+    /**
+     * The name of the campaign
+     */
+    campaignName: z.string(),
+    /**
+     * The type of campaign
+     */
+    campaignType: z.nativeEnum(CampaignType),
+    /**
+     * A short description of the offer/campaign
+     */
+    campaignDescription: z.string().max(250).nullish(),
+    /**
+     * Full campaign content of the offer/campaign in Markdown
+     * This supports large format content such as images, videos, in Markdown format
+     */
+    campaignContent: z.string().nullish(),
+    /**
+     * The featured image for the offer/campaign
+     */
+    campaignImage: z.string().nullish(),
+    /**
+     * Default launding page for the offer/campaign to link users to
+     */
+    landingPage: z.string(),
+    /**
+     * Deprecated: Whether the offer/campaign is blink only or not
+     */
+    blinkOnly: z.boolean().nullish(),
+    /**
+     * The theme of the offer/campaign
+     */
+    offerTheme: z.nativeEnum(OfferTheme).optional(),
+    /**
+     * The background image for the offer/campaign
+     */
+    offerBgImage: z.string().nullish(),
+    /**
+     * The event requirements for the offer/campaign
+     *
+     * {@link EventRequirementConfig}
+     */
+    eventConfig: z.array(EventRequirementConfigSchema),
+    /**
+     * The total number of conversions
+     */
+    conversionCount: z.coerce.number(),
+    /**
+     * The publisher reward type
+     */
+    publisherRewardType: z.nativeEnum(RewardType).optional(),
+    /**
+     * The publisher token address
+     */
+    publisherTokenAddress: z.string().optional(),
+    /**
+     * The payout per conversion for each referral by a publisher
+     */
+    publisherPayoutPerConversion: z.coerce.number().optional(),
+    /**
+     * The user reward type
+     */
+    userRewardType: z.nativeEnum(RewardType).optional(),
+    /**
+     * The user token address
+     */
+    userTokenAddress: z.string().optional(),
+    /**
+     * The payout per conversion for each user
+     */
+    userPayoutPerConversion: z.coerce.number().optional(),
+    /**
+     * The asymmetric/raffle rewards to distribute
+     *
+     * NOTE: The userPubKey and payoutTx are omitted from schema for campaign creation
+     */
+    asymmetricRewards: z.array(AsymmetricRewardSchema.omit({ userPubKey: true, payoutTx: true })).optional(),
+    /**
+     * The lootbox rewards to distribute
+     */
+    lootBoxRewards: LootBoxRewardInputSchema.optional(),
+    /**
+     * The start time of the campaign/offer in Unix timestamp
+     */
+    startTime: z.coerce.number(),
+    /**
+     * The end time of the campaign/offer in Unix timestamp
+     */
+    endTime: z.coerce.number(),
+    /**
+     * The audiences that the offer/campaign is available to
+     */
+    audience: z.string().optional().nullable(),
+    /**
+     * If set, the offer can only be completed by user's who have completed
+     * a conversion for the given offers prior
+     */
+    conversionAudience: ConversionAudienceSchema.optional(),
+    /**
+     * Origin of the campaign
+     */
+    origin: z.nativeEnum(CampaignOrigin).optional(),
 });
 
 /**
